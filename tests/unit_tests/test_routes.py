25cb11ed51055ea7f0cd62dc30f796070bdc63b0
Maxwell Banks <maxwell.banks@colorado.edu>
2022-10-05 23:44:01 -0600

    Fixing again

--- tests/unit_tests/test_routes.py
+++ tests/unit_tests/test_routes.py
@@ -3,7 +3,7 @@ from api.routes import specific_call, specific_call_2
 
 def test_mock_specific_call(mocker):
     mocker.patch(
-        'api.handlers.get_answer_one',
+        'api.handlers.Handlers.get_answer_one',
         return_value={"key_one": "value_one"}
     )
     expected = {"key_one": "value_one"}
@@ -13,7 +13,7 @@ def test_mock_specific_call(mocker):
 
 def test_mock_specific_call_2(mocker):
     mocker.patch(
-        'api.handlers.get_answer_two',
+        'api.handlers.Handlers.get_answer_two',
         return_value={"key_two": "value_two"}
     )
     expected = {"key_two": "value_two"}
